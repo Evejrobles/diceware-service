d66be2cf907b9d8ff60a1babc07d779727ea8f41
Eve <evejrobles@icloud.com>
2018-11-30 11:36:16 -0700

    added oathtoken

--- src/main/java/edu/cnm/deepdive/dicewareservice/DicewareController.java
+++ src/main/java/edu/cnm/deepdive/dicewareservice/DicewareController.java
@@ -4,6 +4,7 @@ import edu.cnm.deepdive.cryptography.ArtifactGenerator;
 import edu.cnm.deepdive.cryptography.PassphraseGenerator;
 import edu.cnm.deepdive.cryptography.WordSource;
 import java.util.Random;
+import javax.activation.MimeType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +18,13 @@ public class DicewareController {
 
   }
 
-  @GetMapping("/diceware")
+  @GetMapping(path ="/diceware", produces = "text/plain")
   public String get(@RequestParam(name = "length", defaultValue = "6") int length) {
     return generater.generate(length);
+  }
+  @GetMapping(path = "/diceware", produces = "application/json")
+  public String[] getJson(@RequestParam(name = "length", defaultValue = "6") int length) {
+    return get(length).split("\\s+");
   }
 
 }
